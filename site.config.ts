8f573f70ba38e200335a473704c9c594e5b897c5
molaod <136430181+molaod@users.noreply.github.com>
2023-12-17 18:54:16 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'bb685e3e0e234026bd0cb2203a4c7804',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -50,16 +50,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+ // navigationStyle: 'default'
+   navigationStyle: 'custom',
+   navigationLinks: [
+     {
+       title: 'About',
+       pageId: '4e9a0fcae72c4175bbe6ddd463fbd873'
+     },
+     {
+       title: 'Contact',
+       pageId: '51dc052d9b56432e8baf965601a51f77'
+     }
+   ]
 })
